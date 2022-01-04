8b16b73c50d757a05ba565441db63295c0dcd8d7
vernikus <jonthan.enriquez.l@gmail.com>
2022-01-03 22:46:41 -0600

    Se agregó la clase 16

---
+++ JS/clase 16/js/main.js
@@ -0,0 +1,12 @@
+
+
+//Altura del window/dom
+console.log(window.innerHeight)
+//Longitud del window/dom
+console.log(window.innerWidth)
+
+//Orientacion de la pantalla  portrait-primary es en vertical y landscape-primary en orizontal
+console.log(screen.orientation.type)
+//Estas son las medidas de la PANTALLA es decir el monitor
+console.log(screen.height)
+console.log(screen.width)
