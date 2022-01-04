4c52c70b90edd1a8d15216eb38ff4399793a998a
vernikus <jonthan.enriquez.l@gmail.com>
2022-01-03 19:31:47 -0600

    Clase 15 usando gitignore

---
+++ JS/clase 15/js/Ecma/main.js
@@ -0,0 +1,8 @@
+console.log('hello world')
+let data = () =>{
+    document.addEventListener('keypress', (event)=>{
+        event.preventDefault
+        event === 'k' ? console.log(`Estas presionando la tecla ${event}`) :console.error('algo salio mal')
+
+    } )
+}
